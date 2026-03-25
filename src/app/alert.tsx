cff3148a95be2158009ff1f138b3828a812b4283
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-03-25 23:16:10 +0800

    update: Page and Alert Function

--- src/app/alert.tsx
+++ src/app/alert.tsx
@@ -5,3 +5,4 @@ import React, { useState } from 'react';
 const BetaAlertDialog = ({ supportEmail = "canoy.john812@gmail.com" }) => {
     const [isOpen, setIsOpen] = useState(true);
 
+    if (!isOpen) return null;
