8be8248ea55ee423e2220145e72a2932386bd02a
JL Canoy <johnlloyd.canoy@1.ustp.edu.ph>
2026-03-27 22:09:04 +0800

    feat: cleaning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavigationBar from "../../components/ui/NavigationBar";
-import PageCompiler from "./pages/PageCompiler";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
